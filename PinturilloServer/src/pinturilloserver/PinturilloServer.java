2399e2b94685fe8269f9e76ffe19777c312ab74f
JulianAhumada86 <julianahumada86@gmail.com>
2019-08-06 09:08:54 -0300

    Server y prueba propio
    Empeze con el servidor y el cliente propio

---
+++ PinturilloServer/src/pinturilloserver/PinturilloServer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pinturilloserver;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author julian
+ */
+public class PinturilloServer {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Servidor c = new Servidor();
+        c.startServer();
+    }
+    
+}
---
+++ Prueba/src/prueba/Prueba.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author julian
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        Cliente c = new Cliente();
+        c.startClient();
+    }
+    
+}
